b1895f887316fe707a543d10b3c2e727f995ee1f
W.R. Ferdian <dr.dits@gmail.com>
2016-01-06 22:00:22 +0700

    add User Dashboard - View

--- functions.php
+++ functions.php
@@ -258,4 +258,26 @@ function save_extra_user_profile_fields($user_id)
   update_usermeta($user_id, 'twitter', $_POST['twitter']);
   update_usermeta($user_id, 'googlep', $_POST['googlep']);
 }
+
+/**
+ * Get excerpt from string
+ *
+ * @param String $str String to get an excerpt from
+ * @param Integer $startPos Position int string to start excerpt from
+ * @param Integer $maxLength Maximum length the excerpt may be
+ * @return String excerpt
+ */
+function getExcerpt($str, $startPos=0, $maxLength=60) {
+	if(strlen($str) > $maxLength) {
+		$excerpt   = substr($str, $startPos, $maxLength-3);
+		$lastSpace = strrpos($excerpt, ' ');
+		$excerpt   = substr($excerpt, 0, $lastSpace);
+		$excerpt  .= '...';
+	} else {
+		$excerpt = $str;
+	}
+
+	return $excerpt;
+}
+
 ?>
---
+++ template-custom-edit-profile.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * Template Name: Custom Template User Profile - edit
+ */
+?>
+
+<div class="user-dashboard">
+  <div class="container">
+
+  <?php while (have_posts()) : the_post(); ?>
+    <?php //get_template_part('templates/page', 'header'); ?>
+    <?php get_template_part('templates/content', 'page'); ?>
+  <?php endwhile; ?>
+
+  <?php if(is_user_logged_in()): ?>
+    <?php $cur_user = wp_get_current_user(); ?>
+    <div class="row user-info-header">
+      <div class="col-md-2 user-photo">
+        <?php echo get_avatar(get_the_author_meta('ID'), 'large'); ?>
+      </div>
+      <div class="col-md-10 user-meta">
+        <h4 class="user-login"><?php echo "".$cur_user->user_login; ?></h4>
+        <div class="info-group">
+            Member since: <?php echo date("d-m-Y", strtotime(get_userdata(get_current_user_id( ))->user_registered)); ?><br>
+          <?php
+            global $wpdb;
+            $user_id = $post->post_author;
+            $where = 'WHERE comment_approved = 1 AND user_id = ' . $user_id ;
+            $comment_count = $wpdb->get_var(
+                "SELECT COUNT( * ) AS total
+                  FROM {$wpdb->comments}
+                  {$where}"
+            );
+            echo 'Total comments: ' . $comment_count;
+          ?>
+        </div>
+        <br>
+        <a class="btn btn-default" href="#">Edit Profile</a>
+      </div>
+     </div><!-- end user info header -->
+
+     <div class="row main-profile">
+       <div class="col-md-9 ">
+         <div class="container-fluid profile-detail">
+
+           <h4 class="title">Profile Detail</h4>
+           <dl class="dl-horizontal">
+             <div class="detail-group">
+               <dt>Name:</dt>          <dd><?php echo get_the_author_meta( 'first_name' ).' '.get_the_author_meta( 'last_name' ); ?></dd>
+             </div>
+             <div class="detail-group">
+               <dt>Gender:</dt>        <dd><?php echo get_the_author_meta( 'gender' ); ?></dd>
+             </div>
+             <div class="detail-group">
+               <dt>Birthday:</dt>      <dd><?php echo get_the_author_meta( 'birthday' ); ?></dd>
+             </div>
+             <div class="detail-group">
+               <dt>Address:</dt>       <dd><?php echo nl2br(get_the_author_meta( 'address' )); ?></dd>
+             </div>
+             <div class="detail-group">
+               <dt>Phone:</dt>         <dd><?php echo get_the_author_meta( 'phone' ); ?></dd>
+             </div>
+             <div class="detail-group">
+               <dt>Email:</dt>         <dd><?php echo get_the_author_meta( 'user_email' ); ?></dd>
+             </div>
+             <div class="detail-group">
+               <dt>Facebook (url):</dt><dd><?php echo get_the_author_meta( 'facebook' ); ?></dd>
+             </div>
+             <div class="detail-group">
+               <dt>Twitter (url):</dt> <dd><?php echo get_the_author_meta( 'twitter' ); ?></dd>
+             </div>
+             <div class="detail-group">
+               <dt>Google+:</dt>       <dd><?php echo get_the_author_meta( 'googlep' ); ?></dd>
+             </div>
+           </dl>
+
+          </div>
+       </div>
+
+       <div class="col-md-3">
+         <div class="container-fluid last-comment">
+           <img class="img img-responsive" src="<?php bloginfo('template_url')?>/assets/images/user-dashboard/cb_user-lastcomment.png" alt="last comment" />
+           <?php
+            $args = array(
+            	'user_id' => 1, // use user_id
+              'number' => 6, // max comment to display
+            );
+            $comments = get_comments($args);
+            echo "<ul>";
+           foreach($comments as $comment) : ?>
+           <li>
+              <div class="comment">
+                <?php
+                  $postID = $comment->comment_post_ID;
+                  $postURL = get_post_permalink($postID);
+
+                  if (function_exists('getExcerpt'))
+                    echo "<a href=".$postURL.">".getExcerpt($comment->comment_content)."</a>";
+                ?>
+              </div>
+              <div class="date">
+                  <?php echo comment_date('d M Y', $comment->comment_ID)  ?>
+              </div>
+            	<!-- echo($comment->comment_author . '<br />' . ); -->
+           </li>
+           <?php endforeach;
+            echo "</ul>";
+           ?>
+       </div>
+       </div>
+     </div><!--  end main profile -->
+     <br>
+     <div class="row popular-post">
+       <div class="container-fluid">
+         <section id="popular-post">
+           <div class="popular-post-header">
+             <img class="img img-responsive" src="<?php bloginfo('template_url')?>/assets/images/smart-news/cb_news-popularpost.png" alt="" />
+           </div>
+
+       		<?php
+       		    if (function_exists('wpp_get_mostpopular'))
+       		        wpp_get_mostpopular( "
+       							limit=4&
+       		        	range=all&
+       		        	post_type=post&
+       		        	wpp_start='<div class=\"row\">'&
+       		        	wpp_end='</div>'&
+       		        	title_by_words=1&
+       		        	thumbnail_width=200&
+       		        	thumbnail_height=130&
+       		        	post_html='<div class=\"col-md-3 pp-content\">{thumb}<div class=\"pp-title\"><b>{title}</b></div></div>'
+       		        	" );
+       		?>
+       	</section>
+       </div>
+     </div>
+  <?php else:
+    auth_redirect(); ?>
+  <?php endif; ?>
+
+ </div><!-- end container -->
+</div><!-- end user dashboard -->
