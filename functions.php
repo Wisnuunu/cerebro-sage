2e272ffa869e5fac35e400d5942737413d7a28c8
Alfi Hanif <inform.knight@gmail.com>
2016-03-04 14:55:14 +0700

    add disqus on ask for expert page

--- functions.php
+++ functions.php
@@ -324,4 +324,17 @@ function running_text($atts) {
 }
 add_shortcode('running_text', 'running_text');
 
+// add disqus on ask for expert page
+function disqus_embed($disqus_shortname) {
+    global $post;
+    wp_enqueue_script('disqus_embed','http://'.$disqus_shortname.'.disqus.com/embed.js');
+    echo '<div id="disqus_thread"></div>
+    <script type="text/javascript">
+        var disqus_shortname = "'.$disqus_shortname.'";
+        var disqus_title = "'.$post->post_title.'";
+        var disqus_url = "'.get_permalink($post->ID).'";
+        var disqus_identifier = "'.$disqus_shortname.'-'.$post->ID.'";
+    </script>';
+}
+
 ?>
